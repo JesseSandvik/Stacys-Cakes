622e6a902a22f96670af208cd6ca5f86436e9f5e
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-15 19:01:44 -0400

    add post form component is functional

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Counter from "./features/counter/Counter";
 import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
 
 const App = () => {
   return (
     <div className='App'>
+      <AddPostForm />
       <PostsList />
     </div>
   );
---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { nanoid } from "@reduxjs/toolkit";
+
+import { postAdded } from "./postsSlice";
+
+const AddPostForm = () => {
+  const dispatch = useDispatch();
+  const initialFormState = {
+    title: "",
+    content: "",
+  };
+
+  const [formData, setFormData] = useState(initialFormState);
+
+  const handleChange = ({ target }) => {
+    console.log(formData);
+    setFormData(
+      (formData) =>
+        (formData = {
+          ...formData,
+          [target.name]: target.value,
+        })
+    );
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.title && formData.content) {
+      dispatch(
+        postAdded({
+          id: nanoid(),
+          ...formData,
+        })
+      );
+      setFormData((formData) => (formData = initialFormState));
+    }
+  };
+
+  return (
+    <section onSubmit={handleSubmit}>
+      <h2>Add A New Post</h2>
+      <form>
+        <label htmlFor='title'>Title: </label>
+        <input
+          id='postTitle'
+          name='title'
+          type='text'
+          value={formData.title ? formData.title : ""}
+          onChange={handleChange}
+        />
+        <label htmlFor='content'>Content: </label>
+        <textarea
+          id='postContent'
+          name='content'
+          value={formData.content ? formData.content : ""}
+          onChange={handleChange}
+        />
+        <button type='submit'>Save Post</button>
+      </form>
+    </section>
+  );
+};
+
+export default AddPostForm;
