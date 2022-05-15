1f9eaa67271ab604a159155d7f4228757edf4527
Jesse Sandvik <jsandvik89@gmail.com>
2022-05-15 18:39:59 -0400

    created posts slice and PostsList component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Counter from "./features/counter/Counter";
+import PostsList from "./features/posts/PostsList";
 
 const App = () => {
   return (
     <div className='App'>
-      <Counter />
+      <PostsList />
     </div>
   );
 };
