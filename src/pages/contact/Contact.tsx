ba6ace81535d6af38c6212db4fba2a348e537d84
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-10 19:42:02 -0400

    slint configured, pages directory, components directory

---
+++ src/pages/contact/Contact.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Contact(): React.ReactElement {
+  return <div />;
+}
+
+export default Contact;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home(): React.ReactElement {
+  return <div />;
+}
+
+export default Home;
