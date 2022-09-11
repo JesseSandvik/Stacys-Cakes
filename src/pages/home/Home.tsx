09965ba47b355fe696f6b45e60299c0c84457d7b
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 01:31:24 -0400

    added images, updated home page layout, basic styling

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,10 +3,19 @@ import React from "react";
 import Heading from "../../components/atoms/heading/Heading";
 import Main from "../../components/organisms/main/Main";
 
+import "./Home.css";
+
 function Home(): React.ReactElement {
   return (
     <Main className="Home">
-      <Heading level={1}>Experience the magic!</Heading>
+      <section className="Home-body-greeting">
+        <Heading level={1}>Experience the magic!</Heading>
+        <p>
+          Stacy&apos;s custom <em>handmade</em> cakes, cupcakes, & treats are
+          prepared <em>fresh</em> from her kitchen, & delivered straight to your
+          doorstep!
+        </p>
+      </section>
     </Main>
   );
 }
