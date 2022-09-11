e1deb1628ff40b9fde6529f3475268f92dd4edfe
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 18:31:47 -0400

    homepage basic layout and design finished

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import Button from "../../components/atoms/button/Button";
 import Heading from "../../components/atoms/heading/Heading";
+import Image from "../../components/atoms/image/Image";
 import Main from "../../components/organisms/main/Main";
 import NavigationLink from "../../components/atoms/navigationLink/NavigationLink";
 
+import WhiteCake from "../../images/cake-white.jpg";
 import "./Home.css";
 
 function Home(): React.ReactElement {
@@ -45,33 +47,36 @@ function Home(): React.ReactElement {
       <section className="Home-body-notice">
         <p>
           <strong>Now serving Nassau and Suffolk county!</strong>
-          <div className="Home-body-notice-social-icons">
-            <a
-              href="mailto: stacyscakesny@gmai.com"
-              rel="noopener noreferrer"
-              target="_blank"
-              title="email"
-            >
-              <i className="fa-solid fa-square-envelope" />
-            </a>
-            <a
-              href="https://www.facebook.com/stacyscakesandcupcakes"
-              rel="noopener noreferrer"
-              target="_blank"
-              title="Facebook"
-            >
-              <i className="fa-brands fa-square-facebook" />
-            </a>
-            <a
-              href="https://www.instagram.com/stacyscakesny"
-              rel="noopener noreferrer"
-              target="_blank"
-              title="Instagram"
-            >
-              <i className="fa-brands fa-instagram-square" />
-            </a>
-          </div>
         </p>
+        <div className="Home-body-notice-social-icons">
+          <a
+            href="mailto: stacyscakesny@gmai.com"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="email"
+          >
+            <i className="fa-solid fa-square-envelope" />
+          </a>
+          <a
+            href="https://www.facebook.com/stacyscakesandcupcakes"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Facebook"
+          >
+            <i className="fa-brands fa-square-facebook" />
+          </a>
+          <a
+            href="https://www.instagram.com/stacyscakesny"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="Instagram"
+          >
+            <i className="fa-brands fa-instagram-square" />
+          </a>
+        </div>
+      </section>
+      <section className="Home-body-image-container">
+        <Image alt="Ornate white frosted cake" src={WhiteCake} />
       </section>
     </Main>
   );
