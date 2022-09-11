86348af68a347a40e1528bbb3cde37dc9e7154b3
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-10 21:07:06 -0400

    <Image /> component accepts id and className passed as props, tests passing

--- src/components/atoms/image/Image.test.tsx
+++ src/components/atoms/image/Image.test.tsx
@@ -14,4 +14,30 @@ describe("<Image />", () => {
 
     expect(testImage).toBeInTheDocument();
   });
+  test("<Image /> component assigned class(className) passed as prop", () => {
+    render(
+      <Image
+        alt="Test Image"
+        className="TestImageClass"
+        src="https://commons.wikimedia.org/wiki/File:Test-Logo.svg"
+      />
+    );
+
+    const testImage = screen.getByRole("img", { name: /test image/i });
+
+    expect(testImage).toHaveClass("TestImageClass");
+  });
+  test("<Image /> component assigned id passed as prop", () => {
+    render(
+      <Image
+        alt="Test Image"
+        id="Test-Image-Id"
+        src="https://commons.wikimedia.org/wiki/File:Test-Logo.svg"
+      />
+    );
+
+    const testImage = screen.getByRole("img", { name: /test image/i });
+
+    expect(testImage).toHaveAttribute("id", "Test-Image-Id");
+  });
 });
--- src/components/atoms/image/Image.tsx
+++ src/components/atoms/image/Image.tsx
@@ -2,11 +2,18 @@ import React from "react";
 
 type Props = {
   alt: string;
+  className?: string;
+  id?: string;
   src: string;
 };
 
-function Image({ alt, src }: Props): React.ReactElement {
-  return <img alt={alt} src={src} />;
+function Image({ alt, className, id, src }: Props): React.ReactElement {
+  return <img alt={alt} className={className} id={id} src={src} />;
 }
+
+Image.defaultProps = {
+  className: undefined,
+  id: undefined,
+};
 
 export default Image;
