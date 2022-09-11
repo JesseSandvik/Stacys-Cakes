f420be702a0cea7c77e19f8d98424e51eb5d2671
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 12:55:10 -0400

    updated Button tests to use jest mock function

--- src/components/atoms/button/Button.test.tsx
+++ src/components/atoms/button/Button.test.tsx
@@ -3,9 +3,7 @@ import Button from "./Button";
 
 describe("<Button />", () => {
   test("Renders the <Button /> component with an initial type of 'button' to be enabled", () => {
-    render(
-      <Button onClick={() => console.log("click")}>this should render</Button>
-    );
+    render(<Button onClick={() => jest.fn()}>this should render</Button>);
 
     const button = screen.getByRole("button", { name: /this should render/i });
 
@@ -15,7 +13,7 @@ describe("<Button />", () => {
   });
   test("<Button /> component has type of 'submit' with type passed as a prop", () => {
     render(
-      <Button onClick={() => console.log("click")} type="submit">
+      <Button onClick={() => jest.fn()} type="submit">
         this should render
       </Button>
     );
@@ -26,7 +24,7 @@ describe("<Button />", () => {
   });
   test("<Button /> component is type 'button' when passed invalid type", () => {
     render(
-      <Button onClick={() => console.log("click")} type="false">
+      <Button onClick={() => jest.fn()} type="false">
         this should render
       </Button>
     );
@@ -37,7 +35,7 @@ describe("<Button />", () => {
   });
   test("<Button /> component assigned class(className) passed as prop", () => {
     render(
-      <Button onClick={() => console.log("click")} className="test-btn">
+      <Button onClick={() => jest.fn()} className="test-btn">
         this should render
       </Button>
     );
@@ -48,7 +46,7 @@ describe("<Button />", () => {
   });
   test("<Button /> component assigned id passed as prop", () => {
     render(
-      <Button onClick={() => console.log("click")} id="test-btn-id">
+      <Button onClick={() => jest.fn()} id="test-btn-id">
         this should render
       </Button>
     );
@@ -59,7 +57,7 @@ describe("<Button />", () => {
   });
   test("<Button /> component disabled when passed 'disabled' as prop", () => {
     render(
-      <Button onClick={() => console.log("click")} disabled>
+      <Button onClick={() => jest.fn()} disabled>
         this should render
       </Button>
     );
