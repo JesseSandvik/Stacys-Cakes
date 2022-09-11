c6d8a45be51ad6d28262c8c5d82f8bd757320184
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 02:10:54 -0400

    further layout and styling for home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Button from "../../components/atoms/button/Button";
 import Heading from "../../components/atoms/heading/Heading";
 import Main from "../../components/organisms/main/Main";
 
@@ -9,12 +10,25 @@ function Home(): React.ReactElement {
   return (
     <Main className="Home">
       <section className="Home-body-greeting">
-        <Heading level={1}>Experience the magic!</Heading>
-        <p>
-          Stacy&apos;s custom <em>handmade</em> cakes, cupcakes, & treats are
-          prepared <em>fresh</em> from her kitchen, & delivered straight to your
-          doorstep!
-        </p>
+        <div className="heading">
+          <Heading level={1}>Experience The Magic!</Heading>
+        </div>
+        <div className="body">
+          <p>
+            Stacy&apos;s custom <em>handmade</em>{" "}
+            <span className="text-accent">cakes</span>,{" "}
+            <span className="text-accent">cupcakes</span>, &{" "}
+            <span className="text-accent">treats</span> are prepared{" "}
+            <em>fresh</em> from her kitchen, & delivered straight to your
+            doorstep!
+          </p>
+        </div>
+        <div className="btn-group">
+          <Button onClick={() => console.log("click")}>view our menu</Button>
+          <Button onClick={() => console.log("click")}>
+            view custom options
+          </Button>
+        </div>
       </section>
     </Main>
   );
