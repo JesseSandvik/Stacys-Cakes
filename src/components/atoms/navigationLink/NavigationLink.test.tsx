0add79065609035bdd025cf581b33862d06a296d
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-11 00:44:37 -0400

    expanded tests, functionality for NavigationLink component

--- src/components/atoms/navigationLink/NavigationLink.test.tsx
+++ src/components/atoms/navigationLink/NavigationLink.test.tsx
@@ -1,3 +1,4 @@
+import userEvent from "@testing-library/user-event";
 import { render, screen } from "../../../test-config/test-library-config";
 import NavigationLink from "./NavigationLink";
 
@@ -8,5 +9,65 @@ describe("<NavigationLink />", () => {
     const testLink = screen.getByRole("link", { name: /testing link/i });
 
     expect(testLink).toBeInTheDocument();
+  });
+  test("<NavigationLink /> component assigned class(className) passed as a prop", () => {
+    render(
+      <NavigationLink className="test-class" path="/">
+        testing link
+      </NavigationLink>
+    );
+
+    const testLink = screen.getByRole("link", { name: /testing link/i });
+
+    expect(testLink).toHaveClass("test-class");
+  });
+  test("Class(className) passed as prop persists while link is 'active'", async () => {
+    render(
+      <NavigationLink className="test-class" path="/test">
+        testing link
+      </NavigationLink>
+    );
+
+    const testLink = await screen.findByRole("link", { name: /testing link/i });
+
+    expect(testLink).not.toHaveClass("active");
+    expect(testLink).toHaveClass("test-class");
+
+    userEvent.click(testLink);
+
+    const testLinkAfterClick = await screen.findByRole("link", {
+      name: /testing link/i,
+    });
+
+    expect(testLinkAfterClick).toHaveClass("active");
+    expect(testLinkAfterClick).toHaveClass("test-class");
+  });
+  test("<NavigationLink /> component assigned id passed as a prop", () => {
+    render(
+      <NavigationLink id="test-id" path="/">
+        testing link
+      </NavigationLink>
+    );
+
+    const testLink = screen.getByRole("link", { name: /testing link/i });
+
+    expect(testLink).toHaveAttribute("id", "test-id");
+  });
+  test("<NavigationLink /> component has class(className) 'active' when current route", async () => {
+    render(<NavigationLink path="/contact">contact link</NavigationLink>);
+
+    const contactLink = await screen.findByRole("link", {
+      name: /contact link/i,
+    });
+
+    expect(contactLink).not.toHaveClass("active");
+
+    userEvent.click(contactLink);
+
+    const contactLinkAfterClick = await screen.findByRole("link", {
+      name: /contact link/i,
+    });
+
+    expect(contactLinkAfterClick).toHaveClass("active");
   });
 });
--- src/components/atoms/navigationLink/NavigationLink.tsx
+++ src/components/atoms/navigationLink/NavigationLink.tsx
@@ -3,11 +3,27 @@ import { NavLink } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
+  className?: string;
+  id?: string;
   path: string;
 };
 
-function NavigationLink({ children, path }: Props): React.ReactElement {
-  return <NavLink to={path}>{children}</NavLink>;
+function NavigationLink({
+  children,
+  className,
+  id,
+  path,
+}: Props): React.ReactElement {
+  return (
+    <NavLink className={className} id={id} to={path}>
+      {children}
+    </NavLink>
+  );
 }
+
+NavigationLink.defaultProps = {
+  className: undefined,
+  id: undefined,
+};
 
 export default NavigationLink;
