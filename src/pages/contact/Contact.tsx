05e7c9f420772ca894eef1c081e17fac6bfd983f
Jesse Sandvik <jsandvik89@gmail.com>
2022-09-12 19:22:06 -0400

    began formatting contact page

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -3,10 +3,14 @@ import React from "react";
 import Heading from "../../components/atoms/heading/Heading";
 import Main from "../../components/organisms/main/Main";
 
+import "./Contact.css";
+
 function Contact(): React.ReactElement {
   return (
     <Main className="Contact">
       <Heading level={1}>Contact</Heading>
+      <section className="Contact-left-panel" />
+      <section className="Contact-right-panel" />
     </Main>
   );
 }
