d47a12beb0009598fc6d0a617fd662af3ac8c72c
Jesse Sandvik <jsandvik89@gmail.com>
2022-10-15 15:35:09 -0400

    updated styling/formatting

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,10 +3,15 @@ import React from "react";
 import Heading from "../../components/atoms/heading/Heading";
 import Main from "../../components/organisms/main/Main";
 
+import "./About.css";
+
 function About(): React.ReactElement {
   return (
     <Main className="About">
-      <Heading level={1}>About</Heading>
+      <div className="About-heading">
+        <Heading level={1}>About Us</Heading>
+      </div>
+      <div className="About-body" />
     </Main>
   );
 }
