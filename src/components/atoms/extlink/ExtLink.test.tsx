4c12cd39da310c2aae5625a114d9f485afb95cda
Jesse Sandvik <jsandvik89@gmail.com>
2022-11-19 14:54:21 -0500

    ExtLink component, expanded test coverage

--- src/components/atoms/extlink/ExtLink.test.tsx
+++ src/components/atoms/extlink/ExtLink.test.tsx
@@ -2,11 +2,47 @@ import { render, screen } from "@testing-library/react";
 import ExtLink from "./ExtLink";
 
 describe("<ExtLink />", () => {
-  test("Renders the ExtLink component with an initial type of 'a'", () => {
-    render(<ExtLink href="http://www.google.com">test ext link</ExtLink>);
+  const testHref = "http://www.google.com";
+  test("Renders the ExtLink component", () => {
+    render(<ExtLink href={testHref}>test ext link</ExtLink>);
 
     const extLink = screen.getByRole("link", { name: /test ext link/i });
 
     expect(extLink).toBeInTheDocument();
+  });
+  test("<ExtLink /> component to have href passed as prop", () => {
+    render(<ExtLink href={testHref}>test ext link</ExtLink>);
+
+    const extLink = screen.getByRole("link", { name: /test ext link/i });
+
+    expect(extLink).toBeInTheDocument();
+    expect(extLink).toHaveAttribute("href", testHref);
+    expect(extLink).not.toHaveAttribute("href", "http://www.badurl.com");
+  });
+  test("<ExtLink /> component assigned class(className) passed as prop", () => {
+    render(
+      <ExtLink className="test-class" href={testHref}>
+        test ext link
+      </ExtLink>
+    );
+
+    const extLink = screen.getByRole("link", { name: /test ext link/i });
+
+    expect(extLink).toBeInTheDocument();
+    expect(extLink).toHaveClass("test-class");
+    expect(extLink).not.toHaveClass("bad-test-class");
+  });
+  test("<ExtLink /> component assigned id passed as prop", () => {
+    render(
+      <ExtLink href={testHref} id="test-id">
+        test ext link
+      </ExtLink>
+    );
+
+    const extLink = screen.getByRole("link", { name: /test ext link/i });
+
+    expect(extLink).toBeInTheDocument();
+    expect(extLink).toHaveAttribute("id", "test-id");
+    expect(extLink).not.toHaveAttribute("id", "bad-test-id");
   });
 });
